21e16570b38b1b0dcd483fc00473f277cb82f416
Juneth <zunaidimran20@gmail.com>
2026-04-01 20:16:55 +0530

    Modified some content

--- src/about.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function About() {
-    return (
-        <div className="about">
-            <h1>About Us</h1>
-            <p>We are a team of dedicated noobs.</p>
-        </div>
-    );
-}
