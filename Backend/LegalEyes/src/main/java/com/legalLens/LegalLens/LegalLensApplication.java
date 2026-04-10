8d0e9f361c5be28ddb0de53554000a6112b9c3e2
Juneth <zunaidimran20@gmail.com>
2026-04-10 19:55:29 +0530

    added spring backend basic files with the needed dependencies

---
+++ Backend/LegalEyes/src/main/java/com/legalLens/LegalLens/LegalLensApplication.java
@@ -0,0 +1,13 @@
+package com.legalLens.LegalLens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegalLensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegalLensApplication.class, args);
+	}
+
+}
